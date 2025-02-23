e6079080bde2b79f28cad0740f3c0e475e1bf218
nchopra <naomi.chopra.021@gmail.com>
2025-02-23 20:21:01 +0530

    make static

--- apps/www/app/layout.config.tsx
+++ apps/www/app/layout.config.tsx
@@ -1,4 +1,5 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import Image from 'next/image';
 
 /**
  * Shared layout configurations
@@ -11,21 +12,19 @@ export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
       <>
-        <svg
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-label="Logo"
-        >
-          <circle cx={12} cy={12} r={12} fill="currentColor" />
-        </svg>
-        My App
+        <Image
+          src="/refref-logo.svg"
+          width={24}
+          height={24}
+          alt="RefRef Logo"
+        />
+        RefRef
       </>
     ),
   },
   links: [
     {
-      text: 'Documentation',
+      text: 'Docs',
       url: '/docs',
       active: 'nested-url',
     },
--- apps/www/next.config.mjs
+++ apps/www/next.config.mjs
@@ -4,7 +4,12 @@ const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
+  output: 'export',
   reactStrictMode: true,
+  images: {
+    domains: ['refref.ai'],
+    unoptimized: true
+  }
 };
 
 export default withMDX(config);
