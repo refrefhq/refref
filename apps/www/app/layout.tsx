6f9307820482ea377bb11e971b825038ee616f3a
nchopra <naomi.chopra.021@gmail.com>
2025-02-24 14:36:08 +0530

    add gtm tag

--- apps/www/app/layout.tsx
+++ apps/www/app/layout.tsx
@@ -3,6 +3,7 @@ import { RootProvider } from 'fumadocs-ui/provider';
 import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
 import PlausibleProvider from 'next-plausible';
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const inter = Inter({
   subsets: ['latin'],
@@ -19,6 +20,7 @@ export default function Layout({ children }: { children: ReactNode }) {
           taggedEvents={true}
           hash={true}
         />
+        <GoogleTagManager gtmId="GTM-MZXNF5TX" />
       </head>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
