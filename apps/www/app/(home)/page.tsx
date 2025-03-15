3e0e533c259049beb4cc392a932d2529f7e7d67f
nchopra <naomi.chopra.021@gmail.com>
2025-03-15 15:21:05 -0700

    add setup blocks

--- apps/www/app/(home)/page.tsx
+++ apps/www/app/(home)/page.tsx
@@ -5,6 +5,9 @@ import { ValueProp } from "@/components/ui/value-prop";
 import { WhyOSS } from "@/components/ui/why-oss";
 import { FeatureBento1 } from "@/components/ui/feature-bento-1";
 import { FeatureRewardTypes } from "@/components/ui/feature-reward-types";
+import { FeatureListGeneral } from "@/components/ui/feature-list-general";
+import { FeatureIndustryTypes } from "@/components/ui/feature-industry-types";
+import { SetupSteps } from "@/components/ui/setup-steps";
 export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col">
@@ -12,7 +15,10 @@ export default function HomePage() {
       <ValueProp />
       <WhyOSS />
       <FeatureBento1 />
+      <SetupSteps />
       <FeatureRewardTypes />
+      <FeatureIndustryTypes />
+      <FeatureListGeneral />
       <FaqGeneral />
       <Cta1 />
     </main>
