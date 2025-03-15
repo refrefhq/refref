7befd445a2e2103b016a71b7bb04ae3af4a3e2b5
nchopra <naomi.chopra.021@gmail.com>
2025-03-15 14:23:30 -0700

    add reward types

--- apps/www/app/(home)/page.tsx
+++ apps/www/app/(home)/page.tsx
@@ -4,7 +4,7 @@ import Cta1 from "@/components/ui/cta-1";
 import { ValueProp } from "@/components/ui/value-prop";
 import { WhyOSS } from "@/components/ui/why-oss";
 import { FeatureBento1 } from "@/components/ui/feature-bento-1";
-
+import { FeatureRewardTypes } from "@/components/ui/feature-reward-types";
 export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col">
@@ -12,6 +12,7 @@ export default function HomePage() {
       <ValueProp />
       <WhyOSS />
       <FeatureBento1 />
+      <FeatureRewardTypes />
       <FaqGeneral />
       <Cta1 />
     </main>
