4dba458050beb5b2d36b1d5adbdbc5e53a476c15
nchopra <naomi.chopra.021@gmail.com>
2025-03-15 14:04:09 -0700

    add feature bento

--- apps/www/app/(home)/page.tsx
+++ apps/www/app/(home)/page.tsx
@@ -3,6 +3,7 @@ import FaqGeneral from "@/components/ui/faq-general";
 import Cta1 from "@/components/ui/cta-1";
 import { ValueProp } from "@/components/ui/value-prop";
 import { WhyOSS } from "@/components/ui/why-oss";
+import { FeatureBento1 } from "@/components/ui/feature-bento-1";
 
 export default function HomePage() {
   return (
@@ -10,6 +11,7 @@ export default function HomePage() {
       <Hero />
       <ValueProp />
       <WhyOSS />
+      <FeatureBento1 />
       <FaqGeneral />
       <Cta1 />
     </main>
